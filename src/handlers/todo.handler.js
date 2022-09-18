5281387574bb25918c4aad4b480b18159e6df17d
Necip Allef <necipallef@gmail.com>
2022-09-18 17:58:53 +0300

    feat: put api

--- src/handlers/todo.handler.js
+++ src/handlers/todo.handler.js
@@ -29,6 +29,33 @@ router.get('/', authenticationMiddleware(), async (req, res) => {
   res.json({ data: todoList })
 })
 
+router.put('/:id', authenticationMiddleware(), async (req, res) => {
+  const userInfo = req.authentication.userToken.userInfo
+  const todoEntityId = req.params.id
+  const todoEntity = await TodoEntity.findByPk(todoEntityId)
+  if (!todoEntity) {
+    res.status(404).end()
+    return
+  }
+
+  const title = req.body.title
+  const details = req.body.details
+  const dueDatetime = req.body.dueDatetime
+  const reminder = req.body.reminder
+
+  const updatedTodoEntity = {
+    userId: userInfo.id,
+    title,
+    details,
+    dueDatetime,
+    reminder,
+  }
+
+  await TodoEntity.update(updatedTodoEntity, {where: {id: userInfo.id}})
+
+  res.json({data: {... updatedTodoEntity, id: todoEntityId }})
+})
+
 router.delete('/:id', authenticationMiddleware(), async (req, res) => {
   const { id: userId } = req.authentication.userToken.userInfo
   const todoEntityId = req.params.id
